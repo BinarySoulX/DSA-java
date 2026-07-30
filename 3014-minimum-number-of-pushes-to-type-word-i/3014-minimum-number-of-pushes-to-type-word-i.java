6e334d77361cd4d1e779aa9598237caac71aa0c7
md abdur <mdabdur64696@gmail.com>
2026-07-30 22:58:50 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n=word.length();
+
+        if(n<=8){return n;}
+
+        else if(n>8 && n<=16){
+            return 8+(2*(n-8));
+        }
+        else if(n>16 && n<=24){
+            return 8+16+(3*(n-16));
+        }
+        else if(n>24){
+            return 8+16+24+(4*(n-24));
+        }return -1;
+    }
+}

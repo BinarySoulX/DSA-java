f6c7006f9fdac7670d6386137808e992935bc975
md abdur <mdabdur64696@gmail.com>
2026-08-01 23:58:54 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,19 @@
+class Solution {
+    int dp[][];
+    private  int solve(int i,int j,int nums[]){
+        if(i==j){return nums[i];}
+        if (dp[i][j] != Integer.MIN_VALUE){return dp[i][j];}
+        int pickLeft = nums[i] - solve(i + 1, j, nums);
+        int pickRight = nums[j] - solve(i, j - 1, nums);
+
+        return dp[i][j] = Math.max(pickLeft, pickRight);
+    }
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        dp=new int[n][n];
+        for(int row[]:dp){
+            Arrays.fill(row,Integer.MIN_VALUE);
+        }
+        return solve(0,n-1,nums)>=0;
+    }
+}

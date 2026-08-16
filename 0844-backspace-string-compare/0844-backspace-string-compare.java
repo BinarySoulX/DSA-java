2bac12b2bc43966ec7f94717573efd10d84f438e
md abdur <mdabdur64696@gmail.com>
2026-08-17 00:48:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (79.08%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+        while (i >= 0 || j >= 0) {
+            
+            while (i >= 0) {     // Find next valid character in s
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else {break;}
+            }
+            while (j >= 0) {// Find next valid character in t
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else {break;}                    
+            }
+
+            
+            if (i < 0 || j < 0) {// One string has a character, the other doesn't
+                return i == j;
+            }
+
+            // Valid characters are diff
+            if (s.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+            --i;--j;
+        }return true;
+        
+    }
+}

d3e193b4e8b0ac2a3c56c250ce7c5c60755aa980
md abdur <mdabdur64696@gmail.com>
2026-09-10 00:42:09 +0530

    Time: 1 ms (99.36%), Space: 42.8 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {//Pattern:Math
+    public long countCommas(long n) {
+        if (n <= 999) return 0;
+        long totalCommas = 0;
+        long start = 1000;
+
+        while (start <= n) {
+            totalCommas += n - start + 1;
+            start *= 1000;
+        }
+
+        return totalCommas;
+    }
+}

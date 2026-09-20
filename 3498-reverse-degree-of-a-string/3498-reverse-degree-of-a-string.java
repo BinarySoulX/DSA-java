3b76fe91b232786f5a3814b64c5898e332e72209
md abdur <mdabdur64696@gmail.com>
2026-09-20 22:33:04 +0530

    Time: 1 ms (100%), Space: 43.2 MB (99.8%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();++i){
+            ans+= (i+1)* ('z'-(int)s.charAt(i)+1);
+        }return ans;
+    }
+}
